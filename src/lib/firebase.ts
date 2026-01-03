7bbee66bf949b7601302b9640da61240465265b1
Your Name <you@example.com>
2026-01-03 11:55:23 +0100

    fix: hardcode firebase config to resolve api key error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCoKOzHW_tzgTtrH5Ain5wdF6jeaaytkxY",
+    authDomain: "anime-app-6b87e.firebaseapp.com",
+    projectId: "anime-app-6b87e",
+    storageBucket: "anime-app-6b87e.firebasestorage.app",
+    messagingSenderId: "683273629384",
+    appId: "1:683273629384:web:5d7035687a3b92bba4bffd"
 };
 
 // Initialize Firebase
